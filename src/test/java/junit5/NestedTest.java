84b18e817ff820ee551e5a52a6c371edda3e3f5b
Simeao David Lamine <simeaolm@gmail.com>
2020-12-28 21:16:25 -0300

    Nested test-case example

---
+++ src/test/java/junit5/NestedTest.java
@@ -0,0 +1,42 @@
+package junit5;
+
+import org.junit.jupiter.api.*;
+
+@DisplayName("Nested Test")
+public class NestedTest {
+
+    String service;
+
+    @BeforeEach
+    void setUp(){
+        service = "Service 1";
+        System.out.println("outer Before Each");
+    }
+
+    @Test
+    @DisplayName("outer testEquals test-case")
+    void testEquals(){
+        Assertions.assertEquals("Service 1", service);
+    }
+
+    @Nested
+    @DisplayName("Nested inner class")
+    class NestedClass{
+
+        String service2;
+
+        @BeforeEach
+        void setUp(){ //Nested Before each will be executed after the outer
+            service2 = "Service 2";
+            System.out.println("Nested Before Each");
+        }
+
+        @Test
+        @DisplayName("Inner testEquals test-case")
+        void testEquals(){
+            Assertions.assertNotEquals(service, service2);
+            Assertions.assertEquals("Service 2", service2);
+        }
+
+    }
+}
